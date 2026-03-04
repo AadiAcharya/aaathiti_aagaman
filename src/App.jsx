5e9da74c5b82a5ece1071043369e8fb2bb1bfea6
AadityaAcharya156 <131011277+AadiAcharya@users.noreply.github.com>
2026-03-04 13:59:12 +0545

    react setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/home/Home'
+import Contact from './components/contact/Contact'
+import About from './components/about/About'
+import Project from './components/projects/Project'
+
 
 function App() {
 
   return (
-    <>
-    <h2>hi</h2>
-    </>
+   <BrowserRouter>
+   <nav>
+    <Link to='/' >Home</Link>
+    <Link to='/' >About</Link>
+    <Link to='/' >Projects</Link>
+    <Link to='/' >Contact</Link>
+   
+   </nav>
+   <Routes>
+    <Route path='' element={<Home/>} />
+    <Route path='/projects' element={<Project/>} />
+    <Route path='/contacts' element={<Contact/>} />
+    <Route path='/about' element={<About/>} />
+   </Routes>
+   </BrowserRouter>
   )
 }
 
