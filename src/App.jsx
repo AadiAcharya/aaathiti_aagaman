d99fe6b0cdefcd64e8c502e29200c70bcc51e872
AadityaAcharya156 <131011277+AadiAcharya@users.noreply.github.com>
2026-03-05 19:11:26 +0545

    navbar made

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
-import './App.css'
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
-import Home from './components/home/Home'
-import Contact from './components/contact/Contact'
-import About from './components/about/About'
-import Project from './components/projects/Project'
-
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/home/Home";
+import Contact from "./components/contact/Contact";
+import About from "./components/about/About";
+import Project from "./components/projects/Project";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-   <BrowserRouter>
-   <nav>
-    <Link to='/' >Home</Link>
-    <Link to='/' >About</Link>
-    <Link to='/' >Projects</Link>
-    <Link to='/' >Contact</Link>
-   
-   </nav>
-   <Routes>
-    <Route path='/' element={<Home/>} />
-    <Route path='/projects' element={<Project/>} />
-    <Route path='/contacts' element={<Contact/>} />
-    <Route path='/about' element={<About/>} />
-   </Routes>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <nav className="bg-background mx-40 text-white max-w-5xl flex p-[32px 0 8px 0] items-end justify-between">
+        <div>
+          <Link to="/">herman</Link>
+        </div>
+        <div className="flex gap-8">
+          <Link to="/">
+            <span className="text-primary">#</span>
+            <span>home</span>
+          </Link>
+          <Link to="/about">
+            <span className="text-primary">#</span>
+            <span>about</span>
+          </Link>
+          <Link to="/projects">
+            <span className="text-primary">#</span>
+            <span>project</span>
+          </Link>
+          <Link to="/contact">
+            <span className="text-primary">#</span>
+            <span>contact</span>
+          </Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/nav" element={<Navbar />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
