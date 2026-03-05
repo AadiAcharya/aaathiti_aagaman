8073ee0a0fcb68ea8e03fcbaf785e159f22ef27a
AadityaAcharya156 <131011277+AadiAcharya@users.noreply.github.com>
2026-03-05 23:22:22 +0545

    --

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
           </Link>
           <Link to="/contact">
             <span className="text-primary">#</span>
-            <span>contacts</span>
+            <span>contact</span>
           </Link>
         </div>
       </nav>
