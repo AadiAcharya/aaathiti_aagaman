84cd965461f24c3c34bdab57888621e0ece86457
AadityaAcharya156 <131011277+AadiAcharya@users.noreply.github.com>
2026-03-29 20:33:59 +0545

    app section updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Properties from "./components/Properties";
+import Account from "./components/Account";
+import Hosting from "./components/Hosting";
+import Messages from "./components/Messages";
+import Rooms from "./components/Rooms";
+import Search from "./components/Search";
+import Wishlist from "./components/Wishlist";
 import AddProperty from "./components/AddProperty/AddProperty.jsx";
 import AddProperty2 from "./components/AddProperty/AddProperty2.jsx";
 
@@ -14,8 +20,14 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/properties" element={<Properties />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/hosting" element={<Hosting />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/rooms" element={<Rooms />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/wishlist" element={<Wishlist />} />
             <Route path="/add-property" element={<AddProperty />} />
-            <Route path="/add-proop" element={<AddProperty2 />} />
+            <Route path="/add-property2" element={<AddProperty2 />} />
           </Routes>
         </main>
       </div>
