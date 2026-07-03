390541c3bd2cda096635fd7665fe7dc5257e2f5e
sparshalc <imailsparsha@gmail.com>
2026-07-03 21:59:47 +0545

    revert server port config!

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const app = require('./app');
 
 connectDB();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
